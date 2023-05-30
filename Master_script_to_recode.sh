c1348027ef9974220b72818facc19c1a3065be47
Leonardo Hernandez <leo.qro@comunidad.unam.mx>
2023-05-29 20:54:57 -0600

    this script now does the whole recoding from lof matrix, a FAM file and a annotation file

--- Master_script_to_recode.sh
+++ Master_script_to_recode.sh
@@ -29,9 +29,16 @@ external_FAM_file="/mnt/Guanina/cvan/data/Keloids_F2/Analysis/leo_analysis/20230
 #define the path to the python script
 python_script="/mnt/Guanina/cvan/data/Keloids_F2/Analysis/leo_analysis/20230508_burden_analysis/recoding_from_a_modified_matrix.py"
 
+#this is the last tmp file that will be used to recode the lof matrix file, is the output file
+tmpfile4=$(mktemp temporary_file.XXXXXXXXXX3)
 module load python38/3.8.3
-python3 $python_script $gene_annotation_file $tmpfile3 $external_FAM_file $outfile
+python3 $python_script $gene_annotation_file $tmpfile3 $external_FAM_file $tmpfile4
 module unload python38/3.8.3
+
+#now that we have completed the recoding of the lof matrix file, we need to recode the ped file
+#in order to do that we will exchange every "0" for "0 0", every "1" for "0 1" and every "2" for "1 1"
+awk 'BEGIN{FS=OFS=" "} {if(NR>1){for(i=7;i<=NF;i++){if($i==0) $i="0 0"; else if($i==1) $i="0 1"; else if($i==2) $i="1 1"}} print}' $tmpfile4 > $outfile
+
 
 #this command will delete the temporary files
 rm $tmpfile
