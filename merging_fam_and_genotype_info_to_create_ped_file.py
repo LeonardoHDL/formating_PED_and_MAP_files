a6da3cb9b833e581d5e479b4fea896ac22e0304c
Leonardo Hernandez <leo.qro@comunidad.unam.mx>
2023-05-29 19:25:16 -0600

    this script merges a genotype file with a fam file to create a ped file

--- merging_fam_and_genotype_info_to_create_ped_file.py
+++ merging_fam_and_genotype_info_to_create_ped_file.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 import sys
+
+#this script is meant to merge the fam file with the genotype file to create a ped file
+#fam file is a external  file that contains the family information
+#genotype file is the file that contains the genotype information, it can be a LOF matrix
 fid_iid_external=sys.argv[1]
 fid_and_iid = pd.read_table(fid_iid_external,sep=' ',names=['FID','IID','motherID', 'FatherID', 'SEX', 'PHENO'])
 genotype_file=sys.argv[2]
 geno_file=pd.read_table(genotype_file, sep=' ',header=None)
-colnames=geno_file.columns.tolist()
-colnames[0]='IID'
-geno_file.columns=colnames
 geno_and_fam=pd.merge(geno_file,fid_and_iid,how='inner',on=['IID'])
 geno_and_fam_col_names=geno_and_fam.columns.tolist()
 new_col_order = ['FID','IID','FatherID','motherID','SEX', 'PHENO']+geno_and_fam_col_names[:-5]
